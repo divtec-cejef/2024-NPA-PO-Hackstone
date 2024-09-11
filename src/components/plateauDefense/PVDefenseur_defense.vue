83d566e583c61064f14b6dc0dd84af80569596b2
fridluc <118963899+fridluc@users.noreply.github.com>
2024-09-11 08:35:08 +0200

    ajout du composant des PV

---
+++ src/components/plateauDefense/PVDefenseur_defense.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="defense_pvDefenses">
+    <img v-for="n in 5" :key="n" src="../../img/coeur.webp" alt="Heart" class="defense_pvDefense">
+  </div>
+</template>
+
+<style scoped>
+.defense_pvDefenses {
+  display: flex;
+  justify-content: center;
+  gap: 5px;
+  position: absolute;
+  bottom: 50px;
+  left: 50px;
+}
+
+.defense_pvDefense {
+  width: 70px; /* Taille des cœurs */
+  height: 70px;
+}
+</style>
